717909efa0e403e1bc0ae6755e32d5599ebb8e0c
JairAntonio22 <jair.loranca@gmail.com>
2022-05-12 16:17:45 -0500

    new error function

--- models.py
+++ models.py
@@ -19,6 +19,15 @@ from sklearn.ensemble import (
 from sklearn import metrics
 
 
+def get_error(model, X, y, percent):
+    pred = model.predict(X)
+
+    err = np.abs(y - pred) / y
+    err = err[err <= percent]
+
+    return len(err) / len(y)
+
+
 def test_models(X, y):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=.80, random_state=42
@@ -35,20 +44,24 @@ def test_models(X, y):
         ('ada boost', AdaBoostRegressor()),
     ]
 
-    scores = [['modelo', 'score']]
+    scores = [['modelo', 'score', 'err 5%', 'err 10%', 'err 15%']]
 
     for name, clf in clfs:
         clf.fit(X_train, y_train)
         score = clf.score(X_test, y_test)
-        scores.append([name, score])
-        y_pred = clf.predict(X_test)
+
+        err_5 = get_error(clf, X_test, y_test, 0.05)
+        err_10 = get_error(clf, X_test, y_test, 0.10)
+        err_15 = get_error(clf, X_test, y_test, 0.15)
+
+        scores.append([name, score, err_5, err_10, err_15])
 
     print(tabulate(scores, headers='firstrow', showindex=True))
 
 
 def main():
     df = pd.read_csv('beer_reviews_updated.csv')
-    df = df.head(10000)
+    df = df.head(10_000)
 
     columns_to_drop = [
         'beer_name', 'brewery_id', 'review_time', 'beer_beerid',
